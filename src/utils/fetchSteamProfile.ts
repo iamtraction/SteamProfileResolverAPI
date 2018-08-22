3baa8370c44e4c60bdf62c9244c35862fd532251
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-08-22 10:46:37 +0000

    utils: add fetchSteamProfile util module

---
+++ src/utils/fetchSteamProfile.ts
@@ -0,0 +1,26 @@
+import * as request from "request-promise-native";
+
+/**
+ * Fetches a user profile from Steam and then returns the XML.
+ * @module fetchSteamProfile
+ * @param {string} vanityName - The vanity name of the user
+ * @returns {string} The user's profile in XML
+ */
+export = (vanityName: string) => {
+  return new Promise<string>(async (resolve, reject) => {
+    try {
+      let url = `https://steamcommunity.com/id/${vanityName}`;
+      let options: request.RequestPromiseOptions = {
+        qs: {
+          xml: 1
+        }
+      };
+      let response = await request.get(url, options);
+
+      resolve(response);   
+    }
+    catch (error) {
+      reject(error);
+    }
+  });
+}
